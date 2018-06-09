ce29993ddc01d99a3682297625749987d302870c
Jitel <git.zamkovec@bk.ru>
2018-06-09 09:05:18 +0300

    node modules

---
+++ ico/src/Header.js
@@ -0,0 +1,38 @@
+
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import "./Header.css";
+
+
+class Header extends Component {
+
+    render() {
+        return (
+            <header className="Header">
+                <div>
+                    <nav>
+                        <ul>
+                            <Link to="/"><img className="Group" src={group}/>
+                            </Link>
+                            <Link to="/">
+                                <button className="Rectangle-5"><a className="Deposit">Deposit</a></button>
+                            </Link>
+                            <Link to="transfer">
+                                <button className="Rectangle-5-Copy"><a className="Transfer">Transfer</a></button>
+                            </Link>
+                            <Link to="Withdrawal">
+                                <button className="Rectangle-5-Copy"><a className="Withdrawal">Witdrawal</a></button>
+                            </Link>
+                            <Link to="history">
+                                <button className='Rectangle-5-Copy'><a className="History">History</a></button>
+                            </Link>
+                        </ul>
+                    </nav>
+                </div>
+
+            </header>
+        )
+    }
+}
+
+export default Header;
